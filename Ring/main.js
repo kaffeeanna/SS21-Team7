06f3971fdb456b886240f3f05ecf56122ce4af7e
yogspace <58942177+yogspace@users.noreply.github.com>
2021-07-10 14:47:33 +0200

    you can  setup a new training

--- Ring/main.js
+++ Ring/main.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 const { saveTraining, getTraining } = require("./src/js/training");
 const WebSocketClient = require("websocket").client;
-const EventEmitter = require('events');
+const EventEmitter = require("events");
 const express = require("express");
 const path = require("path");
-const http = require("http");
 
 const app = express();
 const myEmitter = new EventEmitter();
@@ -15,120 +14,103 @@ let training;
 //max: 192.168.178.20
 const ip = "192.168.178.20";
 
-
 const client = new WebSocketClient();
 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
+client.on("connectFailed", function (error) {
+  console.log("Connect Error: " + error.toString());
 });
 
-client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            myEmitter.emit('getData', JSON.parse(message.utf8Data));
-        }
-    });
-    
-    myEmitter.on("sendData", (data) => {
-        console.log(data);
-        connection.sendUTF(data);
-        training = null;
-    })
-});
-
-myEmitter.on('getData', (data) => {
-    switch (data.msg) {
-        case "get":
-        sendData();
-        break;
-        case "send":
-        useData(data.data);
-        break;
-        default:
-        console.log("something unexpected happened while the case sended something to the ring")
-        break;
+client.on("connect", function (connection) {
+  console.log("WebSocket Client Connected");
+  connection.on("error", function (error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on("close", function () {
+    console.log("echo-protocol Connection Closed");
+  });
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      myEmitter.emit("getData", JSON.parse(message.utf8Data));
     }
+  });
+
+  myEmitter.on("sendData", (data) => {
+    console.log(data);
+    connection.sendUTF(data);
+    training = null;
+  });
+});
+
+myEmitter.on("getData", (data) => {
+  switch (data.msg) {
+    case "get":
+      sendData();
+      break;
+    case "send":
+      useData(data.data);
+      break;
+    default:
+      console.log(
+        "something unexpected happened while the case sended something to the ring"
+      );
+      break;
+  }
 });
 
 async function sendData() {
-let training = await getTraining();
-let sendObj = JSON.stringify(training);
-myEmitter.emit("sendData", sendObj);
-console.log("sended back");
+  let training = await getTraining();
+  let sendObj = JSON.stringify(training);
+  myEmitter.emit("sendData", sendObj);
+  console.log("sended back");
 }
-
-
 
 async function useData(data) {
-console.log("do something");
-training = data;
-// training.objectList[0].randomWord = await getRandomWord();
-// training.name = "HALALALALALA";
-//edit training
-
-// if (training.alreadyKnown){
-    //scanObject
-    //isInformationToRemember?
-        //displayInformationToRemember
-    //getData
-
-// }
-// else {
-    //scanObject
-    //isInformationToRemember?
-        //displayInformationToRemember
-    //createRandomWord
-    //displayRandomWord
-    //setData
-// }
-
-saveTraining(training);
+  console.log("do something");
+  training = data;
+  saveTraining(training);
 }
 
-
-
 // app.all("/",(req, res) => {
-    // res.render("waiting");
-    // res.sendFile(path.join(__dirname + '/index.html', {name}));
-    // startRecording();
-    
+// res.render("waiting");
+// res.sendFile(path.join(__dirname + '/index.html', {name}));
+// startRecording();
+
 // });
 // app.all("/training", (req, res) => {
-    // res.render("index", {training});
+// res.render("index", {training});
 // });
 
 // app.all("/rec", async (req, res) => {
-    // startRecording();
-    // res.render("index");
-    // let name = 12;
-    // res.sendFile(path.join(__dirname + '/index.html', {name}));
+// startRecording();
+// res.render("index");
+// let name = 12;
+// res.sendFile(path.join(__dirname + '/index.html', {name}));
 
 // });
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    // res.send("index.html");
-    res.sendFile(path.join(__dirname + '/index.html'));
-  });
+  // res.send("index.html");
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 app.get("/data", (req, res) => {
-    res.send(training);
+  res.send(training);
 });
 
-app.post("/ressource", (req, res) => {
-console.log(req.body);
+app.post("/ressource", async (req, res) => {
+  // console.log("new data received from ring express" + req);
+  let training = await getTraining();
+
+  // console.log(req.body);
+  // console.log(req.body.id);
+  //     console.log(training);
+  training.objectList[req.body.id] = req.body;
+  saveTraining(training);
+
+  // console.log(res);
 });
-
-
-
 
 // app.post("/training", function (req, res) {
 //     res.send();
@@ -146,7 +128,7 @@ app.listen(3002);
 // const browserServer = http.createServer();
 // browserServer.listen(3002);
 
-console.log("listening on http://localhost:3002")
+console.log("listening on http://localhost:3002");
 
-client.connect('ws://' + ip + ':8080/', 'echo-protocol');
+client.connect("ws://" + ip + ":8080/", "echo-protocol");
 console.log("client listening on " + ip);
--- Ring/public/web.js
+++ Ring/public/web.js
@@ -5,146 +5,199 @@ const showObjectContainer = document.getElementById("showObject");
 const getRandomWordContainer = document.getElementById("getRandomWord");
 const getMessageContainer = document.getElementById("getMessage");
 
-const player = document.getElementById('player');
-const canvas = document.getElementById('canvas');
+const player = document.getElementById("player");
+const canvas = document.getElementById("canvas");
 const randomWord = document.getElementById("randomWord");
-const captureVideoBtn = document.getElementById('captureVideoBtn');
+const captureVideoBtn = document.getElementById("captureVideoBtn");
 
 const button = document.getElementById("btn");
 const content = document.getElementById("content");
+// let oldData;
+
 let buttonStatus = "newObj";
 let training;
+let id = 0;
+let newObject = {
+  id: null,
+  alreadyKnown: null,
+  informationToRemember: null,
+  randomWord: null,
+  audioData: null,
+  objectData: null,
+};
 
 scanObjContainer.style.display = "none";
 showObjectContainer.style.display = "none";
 getRandomWordContainer.style.display = "none";
 getMessageContainer.style.display = "none";
 
-function updateContent(){
-    setInterval(async () => {
-        const response = await fetch("/data", {method: "GET" });
-        const isContentEmpty = response.headers.get("Content-Length") === "0";
+function updateContent() {
+  setInterval(async () => {
+    const response = await fetch("/data", { method: "GET" });
+    const isContentEmpty = response.headers.get("Content-Length") === "0";
 
-        if (isContentEmpty){ 
-            updateHTML("no-content"); 
-            return 
-        }
+    if (isContentEmpty) {
+      updateHTML("no-content");
+      return;
+    }
 
-        const data = await response.json();
-        updateHTML(data);
-        training = data;
-    }, 1000);
+    const data = await response.json();
+    // alreadyGotContent
+    // if (data !== oldData){
+    updateHTML(data);
+    training = data;
+    // }
+    // oldData = data;
+  }, 1000);
 }
 
 updateContent();
 
-function updateHTML(training){
-    if (training === "no-content"){
-        headline.innerHTML = "no training";
-        content.style.display = "none";
-    } else {
-        headline.innerHTML = training.name;
-        content.style.display = "inherit";
-        // buttonStatus = "newObj";
-        // btn.innerHTML = "neues Objekt";
-    }
+function updateHTML(training) {
+  if (training === "no-content") {
+    headline.innerHTML = "no training";
+    content.style.display = "none";
+    scanObjContainer.style.display = "none";
+    showObjectContainer.style.display = "none";
+    getRandomWordContainer.style.display = "none";
+    getMessageContainer.style.display = "none";
+    buttonStatus = "newObj";
+    button.innerHTML = "weiter";
+  } else {
+    headline.innerHTML = training.name;
+    content.style.display = "inherit";
+    // console.log(training);
+    // buttonStatus = "newObj";
+    // btn.innerHTML = "neues Objekt";
+  }
 }
 
-function scanImage(){
-    return new Promise((resolve) => {
-
+function scanImage() {
+  return new Promise((resolve) => {
     const constraints = {
-        video: true,
+      video: true,
     };
-        // Attach the video stream to the video element and autoplay.
-        navigator.mediaDevices.getUserMedia(constraints)
-        .then((stream) => {
-            player.srcObject = stream;
-            console.log(stream);
+    // Attach the video stream to the video element and autoplay.
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      player.srcObject = stream;
+      // console.log(stream);
     });
-    captureVideoBtn.addEventListener('click', () => {
-        let context = canvas.getContext('2d');
-        // Draw the video frame to the canvas.
-        context.drawImage(player, 0, 0, canvas.width, canvas.height);
-        imgData = canvas.toDataURL("image/jpeg", 0.5);
-        resolve(imgData);
-        // console.log(imgData);
-        // document.write('<img src="'+img+'"/>');
-        // data = context.getImageData(0, 0, canvas.width, canvas.height);
-        // console.log(data);
-        // context.drawImage(data, 0, 0);
-        // let med = canvas.toDataURL("image/jpeg", 0.5);
-        // console.log(med)
-        });
+    captureVideoBtn.addEventListener("click", () => {
+      let context = canvas.getContext("2d");
+      // Draw the video frame to the canvas.
+      context.drawImage(player, 0, 0, canvas.width, canvas.height);
+      imgData = canvas.toDataURL("image/jpeg", 0.5);
+      newObject.objectData = imgData;
+      resolve(imgData);
+      // console.log(imgData);
+      // document.write('<img src="'+img+'"/>');
+      // data = context.getImageData(0, 0, canvas.width, canvas.height);
+      // console.log(data);
+      // context.drawImage(data, 0, 0);
+      // let med = canvas.toDataURL("image/jpeg", 0.5);
+      // console.log(med)
     });
+  });
 }
 
 btn.addEventListener("click", async () => {
-    switch (buttonStatus) {
-        case "newObj":
-            let image = document.getElementById("displayObject");
-            // btn.innerHTML = "neues Objekt";
-            getObjContainer.style.display = "none";
-            scanObjContainer.style.display = "inherit";
-            canvas.style.display = "none";
-            let img = await scanImage();
-            image.src = img;
-            canvas.style.display = "inherit";
-            scanObjContainer.style.display = "none";
-            buttonStatus = "showObj";
-            button.innerHTML = "weiter";
-            showObjectContainer.style.display = "inherit"; 
-        break;
-        case "showObj":
-            showObjectContainer.style.display = "none";
-            getRandomWordContainer.style.display = "inherit";
-            randomWord.style.display = "none";
-            let randomWordContent = await getRandomWord();
-            randomWord.innerHTML = randomWordContent;
-            randomWord.style.display = "inherit";
-            buttonStatus = "captureAudio";
-            button.innerHTML = "weiter";
-        break;
-        case "captureAudio":
-            getRandomWordContainer.style.display = "none";
-            getMessageContainer.style.display = "inherit";
-            buttonStatus = "start";
-        break;
-        case "start":
-            getMessageContainer.style.display = "none";
-            getObjContainer.style.display = "inherit";
-            buttonStatus = "newObj";
-            button.innerHTML = "neues Objekt scannen";
-        break;
-    }
+  switch (buttonStatus) {
+    case "newObj":
+      newObject.id = id;
+      newObject.alreadyKnown = false;
+      id = id + 1;
+      let image = document.getElementById("displayObject");
+      // btn.innerHTML = "neues Objekt";
+      getObjContainer.style.display = "none";
+      scanObjContainer.style.display = "inherit";
+      canvas.style.display = "none";
+      let img = await scanImage();
+      image.src = img;
+      canvas.style.display = "inherit";
+      scanObjContainer.style.display = "none";
+      buttonStatus = "showObj";
+      button.innerHTML = "weiter";
+      showObjectContainer.style.display = "inherit";
+      break;
+    case "showObj":
+      showObjectContainer.style.display = "none";
+      getRandomWordContainer.style.display = "inherit";
+      randomWord.style.display = "none";
+      let randomWordContent = await getRandomWord();
+      newObject.randomWord = randomWordContent;
+      randomWord.innerHTML = randomWordContent;
+      randomWord.style.display = "inherit";
+      buttonStatus = "captureAudio";
+      button.innerHTML = "weiter";
+      break;
+    case "captureAudio":
+      getRandomWordContainer.style.display = "none";
+      getMessageContainer.style.display = "inherit";
+      buttonStatus = "last";
+      break;
+    case "last":
+      getMessageContainer.style.display = "none";
+      let json = JSON.stringify(newObject);
+      // console.log("sended: " + json);
+      buttonStatus = "start";
+      button.innerHTML = "zum Anfang";
+
+      fetch("/ressource", {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (myJson) {
+          console.log(myJson);
+        });
+      // fetch("/ressource", {method: "POST", body: json}).then(response => {
+      //     console.log(response);
+      // })
+      // buttonStatus = "start";
+      // button.innerHTML = "zum Anfang";
+      break;
+    case "start":
+      getMessageContainer.style.display = "none";
+      getObjContainer.style.display = "inherit";
+      buttonStatus = "newObj";
+      button.innerHTML = "neues Objekt scannen";
+      // training.objectList.push(newObject);
+      // console.log(training.objectList);
+      break;
+  }
 });
 // addNewObjBtn.addEventListener("click", async () => {
 
 // });
 
+async function getRandomWord() {
+  return new Promise(
+    async (resolve) => {
+      const url = `https://random-words-api.vercel.app/word`;
 
-async function getRandomWord(){
-    return new Promise(async (resolve)=>{
-        const url = `https://random-words-api.vercel.app/word`;
-  
-        const response = await fetch(url);
-        if (response.status != 200) {
-          // throw error if response status is not Ok
-          throw Error("RescueTrack error");
-        }
-      
-        const data = await response.json();
-        resolve(data[0].word);
+      const response = await fetch(url);
+      if (response.status != 200) {
+        // throw error if response status is not Ok
+        throw Error("RescueTrack error");
+      }
+
+      const data = await response.json();
+      resolve(data[0].word);
     },
-    (reject)=>{
-        reject("there was an error");
-    });
+    (reject) => {
+      reject("there was an error");
+    }
+  );
 }
 
+// fetch("/ressource", {method: "POST", body: "daten-die-ich-senden-will"}).then(response => {
+//     console.log(response);
+// })
 
-    // fetch("/ressource", {method: "POST", body: "daten-die-ich-senden-will"}).then(response => {
-    //     console.log(response);
-    // })
-
-    //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    //https://expressjs.com/en/guide/routing.html
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+//https://expressjs.com/en/guide/routing.html
